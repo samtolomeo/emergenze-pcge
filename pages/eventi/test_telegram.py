025504a7d432de000430f60fa6bbd894d69a129d
rmarzocchi84 <roberto.marzocchi@gmail.com>
2019-09-23 09:21:13 +0200

    corretto problema telegram

---
+++ pages/eventi/test_telegram.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#   Gter Copyleft 2018
+#   Roberto Marzocchi
+
+import os
+import urllib2 #problema con python3
+import xml.etree.ElementTree as et
+
+import psycopg2
+from conn import *
+
+import datetime
+import telepot
+
+import config
+# Il token è contenuto nel file config.py e non è aggiornato su GitHub per evitare utilizzi impropri
+TOKEN=config.TOKEN
+
+bot = telepot.Bot(TOKEN)
+
+conn = psycopg2.connect(host=ip, dbname=db, user=user, password=pwd, port=port)
+curr = conn.cursor()
+conn.autocommit = True
+
+query_chat_id= "SELECT telegram_id from users.v_utenti_sistema where telegram_id !='' and telegram_attivo='t' and (id_profilo='1' or id_profilo ='2' or id_profilo ='3') ;"
+curr.execute(query_chat_id)
+lista_chat_id = curr.fetchall() 
+#print("Print each row and it's columns values")
+for row in lista_chat_id:
+    chat_id=row[0]
+    print(chat_id)
+    bot.sendMessage(chat_id, "Nuovo bollettino Protezione civile!")
+    bot.sendMessage(chat_id, messaggio)
