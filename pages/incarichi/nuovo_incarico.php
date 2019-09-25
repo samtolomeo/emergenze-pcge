5145ce4299b7d0a209e4e88e4986e197bae5cb04
rmarzocchi84 <roberto.marzocchi@gmail.com>
2019-09-25 08:21:41 +0200

    sistemate mail

--- pages/incarichi/nuovo_incarico.php
+++ pages/incarichi/nuovo_incarico.php
@@ -234,7 +234,7 @@ $mail->Subject = 'Urgente - Nuovo incarico dalla Protezione Civile del Comune di
 //convert HTML into a basic plain-text alternative body
 $body =  'Hai ricevuto questo messaggio in quanto è stato assegnato un nuovo incarico alla seguente Unità  operativa 
  '.$uo_descrizione.'. <br> Ti preghiamo di non rispondere a questa mail, ma di visualizzare i dettagli dell\'incarico accedendo 
- con le tue credenziali al nuovo <a href="http://192.168.153.110/emergenze/pages/dettagli_incarico.php?id='.$id_incarico.'" " > Sistema di Gestione delle Emergenze </a> del Comune di Genova.
+ con le tue credenziali al nuovo <a href="https://emergenze.comune.genova.it/emergenze/pages/dettagli_incarico.php?id='.$id_incarico.'" " > Sistema di Gestione delle Emergenze </a> del Comune di Genova.
  <br> <br> Protezione Civile del Comune di Genova. <br><br>--<br> Ricevi questa mail  in quanto il tuo indirizzo mail è registrato a sistema. 
  Per modificare queste impostazioni è possibile inviare una mail a salaemergenzepc@comune.genova.it inoltrando il presente messaggio. Ti ringraziamo per la preziosa collaborazione.';
 
--- pages/informativa_privacy_mail.php
+++ pages/informativa_privacy_mail.php
@@ -2,10 +2,10 @@
 
 $body = $body .'<br><br>--<br>
  INFORMATIVA (art. 13, Dlgs 196/2003): Le informazioni contenute nella presente comunicazione (compreso allegati) sono riservate, 
- non soggette a divulgazione e, comunque, destinate esclusivamente alle persone, società o enti sopraindicati. 
- La diffusione, distribuzione e/o copiatura del documento trasmesso da parte di qualsiasi soggetto diverso dal destinatario è proibita, 
+ non soggette a divulgazione e, comunque, destinate esclusivamente alle persone, societ&agrave� o enti sopraindicati. 
+ La diffusione, distribuzione e/o copiatura del documento trasmesso da parte di qualsiasi soggetto diverso dal destinatario &egrave proibita, 
  sia ai sensi dell\'articolo 616 codice penale che ai sensi del D.Lgs. n. 196/2003. Se non ne siete i corretti destinatari, 
- con la presente siete informati che non vi è assolutamente permessa alcuna divulgazione, comunicazione, copia, distribuzione,
+ con la presente siete informati che non vi &egrave assolutamente permessa alcuna divulgazione, comunicazione, copia, distribuzione,
  o altro uso delle informazioni in essa contenute, si prega pertanto di distruggere il messaggio e di informare il mittente al recapito
  di posta elettronica indicato.';
 
