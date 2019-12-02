c2278db56d0af6670b30f4eda74eaa6f860ec7bc
rmarzocchi84 <roberto.marzocchi@gmail.com>
2019-12-02 10:45:24 +0100

    Test notifiche

---
+++ pages/eventi/test.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#   Gter Copyleft 2018
+#   Roberto Marzocchi
+
+import os
+#import urllib2 #problema con python3
+import urllib.request
+import xml.etree.ElementTree as et
+
+import psycopg2
+from conn import *
+
+conn = psycopg2.connect(host=ip, dbname=db, user=user, password=pwd, port=port)
+curr = conn.cursor()
+conn.autocommit = True
+
+
+
+query_chat_id= "SELECT telegram_id from users.v_utenti_sistema where telegram_id !='' and telegram_attivo='t' and (id_profilo='1' or id_profilo ='2' or id_profilo ='3');"
+curr.execute(query_chat_id)
+lista_chat_id = curr.fetchall() 
+#print("Print each row and it's columns values")
+for row in lista_chat_id:
+    chat_id=row[0]
+    print(chat_id)
