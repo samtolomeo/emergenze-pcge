9938c3b1990081e093ccb699e0976de8b66a8466
rmarzocchi84 <roberto.marzocchi@gmail.com>
2021-05-07 22:31:14 +0200

    messa in sicurezza pagina

--- pages/disattiva_notifiche.php
+++ pages/disattiva_notifiche.php
@@ -2,10 +2,17 @@
 
 require(explode('emergenze-pcge',getcwd())[0].'emergenze-pcge/conn.php');
 
-$cf=$_GET['cf'];
+$cf=pg_escape_string($_GET['cf']);
 
-$query = "UPDATE users.utenti_sistema SET telegram_attivo='f' where matricola_cf='".$cf."';";
-$result = pg_query($conn, $query);
+//$query = "UPDATE users.utenti_sistema SET telegram_attivo='f' where matricola_cf='".$cf."';";
+//$result = pg_query($conn, $query);
+$query = "UPDATE users.utenti_sistema SET telegram_attivo='f' where matricola_cf=$1;";
+$result = pg_prepare($conn, "myquery", $query);
+$result = pg_execute($conn, "myquery", array($cf));
+
+
+echo pg_last_error($conn);
+//exit;
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
