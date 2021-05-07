3910988a4c26989b39fcc463fac209b515933552
rmarzocchi84 <roberto.marzocchi@gmail.com>
2021-05-07 15:28:18 +0200

    sistemato disattiva notifiche

--- pages/disattiva_notifiche.php
+++ pages/disattiva_notifiche.php
@@ -4,7 +4,7 @@ require(explode('emergenze-pcge',getcwd())[0].'emergenze-pcge/conn.php');
 
 $cf=$_GET['cf'];
 
-$query = "UPDATE users.utenti_sistema SET telegram_attivo='f' where matricola_cf=".$cf.";";
+$query = "UPDATE users.utenti_sistema SET telegram_attivo='f' where matricola_cf='".$cf."';";
 $result = pg_query($conn, $query);
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
